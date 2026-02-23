3a8ca4563ef604503ad7e483c0dff4058a2bac43
Gokul <gokul040106@gmail.com>
2026-02-23 21:10:49 +0530

    added netlify configuration

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,25 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     domains: ["lh3.googleusercontent.com", "lh3.googleusercontent.com"]
+//   }
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ["lh3.googleusercontent.com", "lh3.googleusercontent.com"]
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
